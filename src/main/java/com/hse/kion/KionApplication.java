e1d71b5a508f9386a9e1bf8cd7ca9c16f7f0393f
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2022-01-23 20:27:37 +0300

    Init

---
+++ src/main/java/com/hse/kion/KionApplication.java
@@ -0,0 +1,14 @@
+package com.hse.kion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KionApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/hse/kion/KionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hse.kion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
