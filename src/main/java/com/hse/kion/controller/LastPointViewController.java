06145a40ff54484e2227e64010d2bc40cd6dce02
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2022-03-29 21:18:41 +0300

    Подправил

--- src/main/java/com/hse/kion/controller/LastPointViewController.java
+++ src/main/java/com/hse/kion/controller/LastPointViewController.java
@@ -4,10 +4,7 @@ import com.hse.kion.controller.dto.lastPointView.LastPointViewDTO;
 import com.hse.kion.controller.dto.lastPointView.LastPointViewInfoForGetDTO;
 import com.hse.kion.service.LastPointViewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,45 +18,8 @@ import java.util.List;
 public class LastPointViewController {
     private final LastPointViewService lastPointViewService;
 
-    @GetMapping("/lastPointView")
+    @PostMapping("/lastPointView")
     public List<LastPointViewDTO> getLastPointViewsByVideoIds(@RequestBody LastPointViewInfoForGetDTO dto) {
         return lastPointViewService.getLastPointViewsByVideoIds(dto);
-    }
-
-    @GetMapping("/test")
-    public Integer getTest() {
-        String str = "{\n" +
-            "    \"userId\" : 1,\n" +
-            "    \"videoId\" : 4,\n" +
-            "    \"type\" : \"PLAY\",\n" +
-            "    \"videoTime\" : 10\n" +
-            "}";
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .headers("Content-Type", "application/json")
-            .uri(URI.create("http://localhost:8080/event").normalize())
-                .POST(HttpRequest.BodyPublishers.ofString(str))
-                .build();
-
-        System.out.println(request.toString());
-
-        List<Thread> list = new ArrayList<>();
-        for (int j = 0; j < 10; j++) {
-            list.add(new Thread(
-                () -> {
-                    HttpClient client = HttpClient.newHttpClient();
-                    for (int i = 0; i < 1000000; i++) {
-                        try {
-                            client.send(request, HttpResponse.BodyHandlers.ofString());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            ));
-        }
-
-        list.parallelStream().forEach(Thread::start);
-        return 200;
     }
 }
